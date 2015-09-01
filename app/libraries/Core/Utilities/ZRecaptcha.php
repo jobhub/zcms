49d89d280b3b5212aef7ddca52abcdad6ca3ffa9
kimthangatm <kimthangatm@gmail.com>
2015-09-01 09:12:26 +0700

    Update COre

---
+++ app/libraries/Core/Utilities/ZRecaptcha.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ZCMS\Core\Utilities;
+
+use ReCaptcha\ReCaptcha;
+
+require(ROOT_PATH . '/app/libraries/Recaptcha/src/autoload.php');
+/**
+ * Class ZReCaptcha
+ *
+ * @package ZCMS\Core\Utilities
+ */
+class ZReCaptcha extends ReCaptcha
+{
+
+}
